4dd53743f1da53609c5fe661050d6856702f15db
Hollyviar Zalukhu <88412539+hollyviarptr@users.noreply.github.com>
2021-11-19 20:10:35 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "Poppins",
+      },
+      colors: {
+        primary: "#BB1529",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
